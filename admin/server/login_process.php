6f1987dc159120d246ed1ffe2c610ac7aa51907a
ajwildr <ajk519451@gmail.com>
2024-09-16 10:33:02 +0530

    Update login_process.php

--- admin/server/login_process.php
+++ admin/server/login_process.php
@@ -3,6 +3,7 @@ include('connect.php');
 session_start();
 $uname=$_POST['username'];
 $pass=$_POST['password'];
+$user="super_user";
 $sel_query="SELECT pass,event_name from users where user_name='$uname'";
 $execute=$conn->query($sel_query);
 $user_details=$execute->fetch_assoc();
@@ -11,7 +12,7 @@ if($user_details['event_name']=='admin')
 {
     $_SESSION['user']="super_user";
     // header("Location:/takshak/admin/admin.php");
-    echo "<script>window.location.href='/admin/admin.php';</script>";
+    echo "<script>window.location.href='/admin/admin.php?id={$user}';</script>";
 }
 else if($user_details['event_name']==null){
     header("Location:invalid_credentials.php");
