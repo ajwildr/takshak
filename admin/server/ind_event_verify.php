f359b72e90f9894e7d0d284ad20f49681f6e7cc7
ajwildr <ajk519451@gmail.com>
2024-09-16 12:01:52 +0530

    Update ind_event_verify.php

--- admin/server/ind_event_verify.php
+++ admin/server/ind_event_verify.php
@@ -7,12 +7,13 @@ include('connect.php');
 $update_status="UPDATE individual_events set status='verified' where reg_id ='$id'";
 if($conn->query($update_status))
     {
-        if($_SESSION['user']=="super_user")
+         if($_SESSION['user']=="super_user")
             {
-              header("Location:/takshak/admin/admin.php");
+               echo "<script>window.location.href = '/admin/admin.php';</script>";
+    
             }
         else{
-            header("Location:/takshak/admin/coordinator.php");
+            echo "<script>window.location.href = '/admin/coordinator.php';</script>";
         }
     }
 else{
