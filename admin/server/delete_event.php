c717373d58a80f1ca57378be7f098fd511440a64
ajwildr <ajk519451@gmail.com>
2024-09-16 12:34:32 +0530

    Update delete_event.php

--- admin/server/delete_event.php
+++ admin/server/delete_event.php
@@ -4,7 +4,8 @@ $id=$_GET['id'];
 $delete_query="DELETE from event_details  where event_id='$id'";
 echo($delete_query);
 if($conn->query($delete_query))
-    {   header("Location:event_list.php");
+    {   //header("Location:event_list.php");
+        echo "<script>window.location.href = 'event_list.php';</script>";
         
     }
 else{
