b13f695b1d04ef667ca7aebb1c798b52d2369843
ajwildr <ajk519451@gmail.com>
2024-09-17 00:04:55 +0530

    updates

--- registration/valorant_vortex.php
+++ registration/valorant_vortex.php
@@ -145,7 +145,7 @@ if($current_reg<$limit)
                             </div>
                         </div>
                         <div class="form-row">
-                            <div class="name">To Pay: <br><h2>Rs 500/-</h2></div>
+                            <div class="name">To Pay: <br><h2>Rs 300/-</h2></div>
                             <div class="value">
                                 <div class="input-group">
                                     <img src="images\VALORANT VORTEX_qrcode.jpg" width=300px height=400px><br>
