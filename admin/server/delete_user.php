2b0654b9b70251896789eeba489ba259039407db
ajwildr <ajk519451@gmail.com>
2024-09-16 12:49:18 +0530

    Update delete_user.php

--- admin/server/delete_user.php
+++ admin/server/delete_user.php
@@ -4,7 +4,8 @@ $id=$_GET['id'];
 $delete_query="DELETE from users  where id='$id'";
 echo($delete_query);
 if($conn->query($delete_query))
-    {   header("Location:users_list.php");
+    {  // header("Location:users_list.php");
+     echo "<script>window.location.href = 'users_list.php';</script>";
         
     }
 else{
