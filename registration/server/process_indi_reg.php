ddb3984101d018c8ba8b9d81899a271db01c83b0
ajwildr <ajk519451@gmail.com>
2024-09-17 20:22:54 +0530

    Update process_indi_reg.php

--- registration/server/process_indi_reg.php
+++ registration/server/process_indi_reg.php
@@ -10,12 +10,13 @@ $sel=$conn->query($select_strength);
 $event_details=$sel->fetch_assoc();
 $event_name=$event_details['event_name'];
 //taking values from form
+$name=$_POST['name'];
 $clg=$_POST['collegename'];
 $dpt_name=$_POST['department'];
 $mail=$_POST['email'];
 $phn=$_POST['phone'];
 $trans_id=$_POST['transaction'];
-$insert_query="INSERT into individual_events(clg_name,dept_name,mail,phone,transaction_id,event_name,event_id) values('$clg','$dpt_name','$mail','$phn','$trans_id','$event_name','$event_id')";
+$insert_query="INSERT into individual_events(clg_name,dept_name,mail,phone,transaction_id,event_name,event_id,name) values('$clg','$dpt_name','$mail','$phn','$trans_id','$event_name','$event_id','$name')";
 try {
     // Execute the query
     if ($conn->query($insert_query) === TRUE) {
