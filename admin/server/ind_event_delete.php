45788f777e34b7987b09c082cbef55611cfa3045
ajwildr <ajk519451@gmail.com>
2024-09-17 21:14:09 +0530

    Update ind_event_delete.php

--- admin/server/ind_event_delete.php
+++ admin/server/ind_event_delete.php
@@ -4,7 +4,8 @@ include('connect.php');
 $update_status="UPDATE individual_events set status='deleted' where reg_id ='$id'";
 if($conn->query($update_status))
     {
-        header("delete_participant.php");
+        //header("delete_participant.php");
+         echo "<script>window.location.href='delete_participant.php';</script>";
     }
 else{
     echo($conn->error);
