eacddab9b3ec8e9b2b044b5afd854ce75ded472d
ajwildr <ajk519451@gmail.com>
2024-09-17 23:29:29 +0530

    Update check_status.php

--- check_status.php
+++ check_status.php
@@ -1,6 +1,21 @@
 <?php
 echo("hello");
-include('registration/server/connect.php');
+
+// Database connection details
+$hostname = "testing123ajai-server.mysql.database.azure.com";
+$username = "dglktgierk";
+$password = 'EzVMnQnSNI$kJwT3'; // Ensure the password is correct
+$database = "thakshak"; // Ensure this matches the database name
+
+// Initialize MySQLi
+$conn = mysqli_init();
+
+// Connect to the MySQL server
+if (!mysqli_real_connect($conn, $hostname, $username, $password, $database, 3306)) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
                                     $mail = $_POST['mail'];
 
                                     $sel_grp = "SELECT * FROM group_event WHERE mail='$mail'";
