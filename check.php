4d00a74a0e54b4d4d1ff1e342ea0b63f1b4be9a8
ajwildr <ajk519451@gmail.com>
2024-09-17 21:10:17 +0530

    Update check.php

--- check.php
+++ check.php
@@ -142,7 +142,7 @@
                             <div class="container">
                                 <form action="" method="post">
                                     <input type="email" id="email" name="email" placeholder="Enter your registered email ID" required>
-                                    <input  value="Check Status" name="check">
+                                    <input  type="button" value="Check Status" name="check">
                                 </form>
 
                                 <?php
