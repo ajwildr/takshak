556034a0d76d9c2f0cbb81fa628696fb0f9b616f
ajwildr <ajk519451@gmail.com>
2024-09-18 11:53:46 +0530

    Update set_event_limit.php

--- set_event_limit.php
+++ set_event_limit.php
@@ -18,16 +18,17 @@ UPDATE event_limit
 SET current_reg = 0,
     reg_limit = CASE
         WHEN event_id = 1 THEN 80
-        WHEN event_id = 2 THEN 45
+        WHEN event_id = 2 THEN 8
         WHEN event_id = 3 THEN 45
-        WHEN event_id = 4 THEN 60
-        WHEN event_id = 5 THEN 30
+        WHEN event_id = 4 THEN 45
+        WHEN event_id = 5 THEN 60
+        WHEN event_id = 6 THEN 20
         WHEN event_id = 7 THEN 30
-        WHEN event_id = 8 THEN 100
+        WHEN event_id = 8 THEN 30
         WHEN event_id = 9 THEN 100
         ELSE reg_limit
     END
-WHERE event_id IN (1, 2, 3, 4, 5, 7, 8, 9)
+WHERE event_id IN (1, 2, 3, 4, 5,6, 7, 8, 9)
 ";
 
 if (mysqli_query($conn, $sql)) {
