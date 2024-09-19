98ea73ba693b95cbc0b1bf5b92aa694b46db9f4d
ajwildr <ajk519451@gmail.com>
2024-09-19 22:41:46 +0530

    Update event_status.php

--- event_status.php
+++ event_status.php
@@ -14,15 +14,28 @@ if (!mysqli_real_connect($conn, $hostname, $username, $password, $database, 3306
     die("Connection failed: " . mysqli_connect_error());
 }
 
+$mail = $_POST['mail'];
 
-                                    $mail = $_POST['mail'];
+// Use prepared statements to prevent SQL injection for the group event
+$sel_grp = "SELECT * FROM group_event WHERE mail = ?";
+if ($stmt_grp = $conn->prepare($sel_grp)) {
+    $stmt_grp->bind_param("s", $mail); // "s" is for string
+    $stmt_grp->execute();
+    $grp_data = $stmt_grp->get_result();
+    $stmt_grp->close();
+}
 
-                                    $sel_grp = "SELECT * FROM group_event WHERE mail='$mail'";
-                                    $grp_data = $conn->query($sel_grp);
+// Use prepared statements to prevent SQL injection for the individual events
+$sel_ind = "SELECT * FROM individual_events WHERE mail = ?";
+if ($stmt_ind = $conn->prepare($sel_ind)) {
+    $stmt_ind->bind_param("s", $mail); // "s" is for string
+    $stmt_ind->execute();
+    $ind_data = $stmt_ind->get_result();
+    $stmt_ind->close();
+}
 
-                                    $sel_ind = "SELECT * FROM individual_events WHERE mail='$mail'";
-                                    $ind_data = $conn->query($sel_ind);
 ?>
+
 <!doctype html>
 <html class="no-js" lang="zxx">
 
@@ -33,24 +46,9 @@ if (!mysqli_real_connect($conn, $hostname, $username, $password, $database, 3306
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- <link rel="manifest" href="site.webmanifest"> -->
-    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
-    <!-- Place favicon.ico in the root directory -->
-
     <!-- CSS here -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/themify-icons.css">
-    <link rel="stylesheet" href="css/gijgo.css">
-    <link rel="stylesheet" href="css/nice-select.css">
-    <link rel="stylesheet" href="css/animate.css">
-    <link rel="stylesheet" href="css/flaticon.css">
-    <link rel="stylesheet" href="css/slicknav.css">
-
     <link rel="stylesheet" href="css/style.css">
-    <!-- <link rel="stylesheet" href="css/responsive.css"> -->
 
     <style>
         body {
@@ -65,7 +63,6 @@ if (!mysqli_real_connect($conn, $hostname, $username, $password, $database, 3306
             color: rgb(4, 139, 76);
             text-align: center;
         }
-
 
         label {
             display: block;
@@ -124,7 +121,6 @@ if (!mysqli_real_connect($conn, $hostname, $username, $password, $database, 3306
             color: white;
         }
 
-        /* Media query for smaller screens */
         @media (max-width: 600px) {
             h1 {
                 font-size: 24px;
@@ -147,11 +143,6 @@ if (!mysqli_real_connect($conn, $hostname, $username, $password, $database, 3306
                 font-size: 14px;
             }
 
-            .container {
-                padding: 15px;
-            }
-
-            /* Table styling for small screens */
             table, th, td {
                 font-size: 12px;
                 padding: 5px;
@@ -161,7 +152,6 @@ if (!mysqli_real_connect($conn, $hostname, $username, $password, $database, 3306
 </head>
 <body>
 
-    <!-- header-start -->
     <header>
         <div class="header-area ">
             <div id="sticky-header" class="main-header-area">
@@ -183,21 +173,6 @@ if (!mysqli_real_connect($conn, $hostname, $username, $password, $database, 3306
                                             <li><a href="about.html">About</a></li>
                                             <li><a href="#">Check Status</a></li>
                                             <li><a href="admin\login_page.php">Login</a></li>
-                                            
-                                            <!--<li><a href="#">pages <i class="ti-angle-down"></i></a>
-                                                <ul class="submenu">
-                                                    <li><a href="about.html">about</a></li>
-                                                    <li><a href="Program.html">Program</a></li>
-                                                    <li><a href="Venue.html">Venue</a></li>
-                                                    <li><a href="elements.html">elements</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="#">blog <i class="ti-angle-down"></i></a>
-                                                <ul class="submenu">
-                                                    <li><a href="blog.html">blog</a></li>
-                                                    <li><a href="single-blog.html">single-blog</a></li>
-                                                </ul>
-                                            </li>-->
                                             <li><a href="contact.html">Contact</a></li>
                                         </ul>
                                     </nav>
@@ -215,87 +190,70 @@ if (!mysqli_real_connect($conn, $hostname, $username, $password, $database, 3306
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
     </header>
-    <!-- header-end -->
 
-    <!-- slider_area_start -->
     <div class="slider_area">
-        <div class="single_slider  d-flex align-items-center slider_bg_1 overlay">
+        <div class="single_slider d-flex align-items-center slider_bg_1 overlay">
             <div class="container">
                 <div class="row align-items-center justify-content-center">
                     <div class="col-xl-12">
                         <div class="slider_text text-center">
-                            <div class="shape_1 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".2s">
-                                <img src="img/shape/shape_1.svg" alt="">
+                            <h1>Registration Status</h1>
+                            <div class="container">
+                                <form action="">
+                                    <h1>Individual Events</h1>
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>Email ID</th>
+                                                <th>Name</th>
+                                                <th>Event Name</th>
+                                                <th>Status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php while ($ind_event = $ind_data->fetch_assoc()) { ?>
+                                            <tr>
+                                                <td><?php echo $ind_event['mail']; ?></td>
+                                                <td><?php echo $ind_event['name']; ?></td>
+                                                <td><?php echo $ind_event['event_name']; ?></td>
+                                                <td><?php echo $ind_event['status']; ?></td>
+                                            </tr>
+                                            <?php } ?>
+                                        </tbody>
+                                    </table>
+                                    <br><br>
+                                    <h1>Group Events</h1>
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>Email ID</th>
+                                                <th>Name</th>
+                                                <th>Event Name</th>
+                                                <th>Status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php while ($grp_event = $grp_data->fetch_assoc()) { ?>
+                                            <tr>
+                                                <td><?php echo $grp_event['mail']; ?></td>
+                                                <td><?php echo $grp_event['captain_name']; ?></td>
+                                                <td><?php echo $grp_event['event_name']; ?></td>
+                                                <td><?php echo $grp_event['status']; ?></td>
+                                            </tr>
+                                            <?php } ?>
+                                        </tbody>
+                                    </table>
+                                </form>
                             </div>
-                                <h1>Registration Status</h1>
-        
-                                <div class="container">
-                                    <form action="">
-                                        <h1>Individual Events</h1>
-                                        <table>
-                                            <thead>
-                                                <tr>
-                                                    <th>Email ID</th>
-                                                    <th>Name</th>
-                                                    <th>Event Name</th>
-                                                    <th>Status</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                 <?php while ($ind_event = $ind_data->fetch_assoc()) { ?>
-                                               <tr>
-                                            <td><?php echo $ind_event['mail']; ?></td>
-                                            <td><?php echo $ind_event['name']; ?></td>
-                                            <td><?php echo $ind_event['event_name']; ?></td>
-                                            <td><?php echo $ind_event['status']; ?></td>
-                                        </tr>
-                                        <?php } ?>
-                                              
-                                            </tbody>
-                                        </table>
-                                        <br><br>
-
-                                        <h1>Group Events</h1>
-
-                                        <table>
-                                            <thead>
-                                                <tr>
-                                                    <th>Email ID</th>
-                                                    <th>Name</th>
-                                                    <th>Event Name</th>
-                                                    <th>Status</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                 <?php while ($grp_event = $grp_data->fetch_assoc()) { ?>
-                                        <tr>
-                                            <td><?php echo $grp_event['mail']; ?></td>
-                                            <td><?php echo $grp_event['captain_name']; ?></td>
-                                            <td><?php echo $grp_event['event_name']; ?></td>
-                                            <td><?php echo $grp_event['status']; ?></td>
-                                        </tr>
-                                        <?php } ?>
-                                                
-                                            </tbody>
-                                        </table>
-                                    </form>
-                                </div>
-
-                            <div class="shape_2 wow fadeInDown" data-wow-duration="1s" data-wow-delay=".2s">
-                                <img src="img/shape/shape_2.svg" alt="">
-                            </div>
-                
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- slider_area_end -->
 </body>
 </html>
