b0056742087473c9c2f646d608cccb2f9329b165
ajwildr <ajk519451@gmail.com>
2024-09-20 20:35:19 +0530

    Update delete_participant.php

--- admin/server/delete_participant.php
+++ admin/server/delete_participant.php
@@ -1,6 +1,19 @@
 <?php
-include('server/connect.php');
+
 session_start();
+$hostname = "testing123ajai-server.mysql.database.azure.com";
+$username = "dglktgierk";
+$password = 'EzVMnQnSNI$kJwT3'; // Ensure the password is correct
+$database = "thakshak"; // Ensure this matches the database name
+
+// Initialize MySQLi
+$conn = mysqli_init();
+
+// Connect to the MySQL server
+if (!mysqli_real_connect($conn, $hostname, $username, $password, $database, 3306)) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
 
 
 // Set Content Security Policy to prevent execution of any scripts
